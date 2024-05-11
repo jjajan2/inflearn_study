b35e089f769af2dcdfa6552b31a69bd8eb3cafbe
jjajan2 <hje0514@gmail.com>
2024-05-12 03:13:24 +0900

    feat:toastify 메세지 추가

--- day9_budget_calculator/src/App.js
+++ day9_budget_calculator/src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import style from "./App.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [amountForm, setAmountForm] = useState({
@@ -44,21 +46,35 @@ function App() {
 
   function addBudget(event) {
     event.preventDefault();
+
+    if (amountForm.item === "") {
+      return toast.error("지출 항목을 입력해주세요.");
+    }
+    if (amountForm.amount === "") {
+      return toast.error("금액을 입력해주세요.");
+    }
+
     const newList = [...budgetList, amountForm];
     setAmountForm({ id: "", item: "", amount: "" });
     setbudgetList(newList);
     localStorage.setItem("budgetList", JSON.stringify(newList));
+    toast.success("추가 되었습니다.");
   }
 
   function deleteBudget(id) {
     const newList = budgetList.filter((budget) => budget.id !== id);
     setbudgetList(newList);
     localStorage.setItem("budgetList", JSON.stringify(newList));
+    toast.success("삭제 되었습니다.");
   }
 
   function deleteAll() {
+    if (budgetList.length === 0) {
+      return toast.error("내용이 없습니다.");
+    }
     setbudgetList([]);
     localStorage.setItem("budgetList", JSON.stringify([]));
+    toast.success("삭제 되었습니다.");
   }
 
   function handleChangeEditInput(event) {
@@ -74,6 +90,7 @@ function App() {
     setbudgetList(newList);
     setEditId(-1);
     localStorage.setItem("budgetList", JSON.stringify(newList));
+    toast.success("수정 되었습니다.");
   }
 
   function editCancle() {
@@ -83,6 +100,11 @@ function App() {
   return (
     <div className={style.App}>
       <h1 className={style.mainTitle}>예산 계산기</h1>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={true}
+      />
       <form onSubmit={(event) => addBudget(event)}>
         <div className={style.budgetFormBox}>
           <div className={style.budgetFormBoxItem}>
@@ -95,7 +117,7 @@ function App() {
             />
           </div>
           <div className={style.budgetFormBoxItem}>
-            <label>비용</label>
+            <label>금액</label>
             <input
               type="number"
               name="amount"
@@ -176,7 +198,7 @@ function App() {
         </div>
       )}
       <div className={style.totalAmount}>
-        <button class={style.deleteAllButton} onClick={deleteAll}>
+        <button className={style.deleteAllButton} onClick={deleteAll}>
           전체 삭제
         </button>
         <p>
